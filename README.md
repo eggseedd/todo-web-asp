b408feb6291dae0c1954ccb56cd5c2801191b550
Raditya Yusuf <radityayusuf002@gmail.com>
2024-12-13 19:11:46 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# todo-web-asp
+ Todo list Webapp using ASP.NET
---
+++ TodoWebNet/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TodoWebNet.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
